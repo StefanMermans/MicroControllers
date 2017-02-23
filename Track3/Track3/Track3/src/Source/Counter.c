7f35cc3d2dbc2f4f4b7e580e5916034b7e572248
camiel buskens <camiel0910@hotmail.com>
2017-02-23 10:34:05 +0100

    code removed

--- Track3/Track3/Track3/src/Source/Counter.c
+++ Track3/Track3/Track3/src/Source/Counter.c
@@ -10,37 +10,8 @@
 #include <stdio.h>
 #include "Headers/LCD.h"
 
-#define MAX_OVERFLOW 2
-#define REMAINDER_TIMER 31250
-
-volatile int overflow_count = 0;
-
-ISR ( TIMER1_OVF_vect )
-{
-	TCNT1 = REMAINDER_TIMER; // Reload timer with precalculated value
-
-	overflow_count += 1;
-	if(overflow_count >= MAX_OVERFLOW){
-		overflow_count = 0;
-		PORTB ^= (1 << 0) ; // Toggle the LED
-	}	
-}
-
-ISR (TIMER2_COMP_vect)
-{
-	PORTB = TCNT2;
-	};
-
 void counter_init()
 {
-	//DDRB |= (1 << 0) ; // Set LED as output
-	//
-	//TIMSK |= (1 << TOIE1 ); // Enable overflow interrupt
-	//sei () ; // Enable global interrupts
-	//
-	//TCNT1 = REMAINDER_TIMER; // Preload timer with precalculated value
-	//
-	//TCCR1B |= ((1 << CS10 ) | (1 << CS11 )) ; // Set up timer at Fcpu /64
 	DDRD = 0xFF;
 	PORTD = 0x00;
 	TCCR2 = 0x07;
